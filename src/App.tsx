ba08f980801a61ea9f6314f4131bf6665fba6373
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-09-05 14:12:34 +0900

    added the sample code for Apollo Client.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, gql } from "@apollo/client";
+import "./App.css";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(
+    ({
+      id,
+      name,
+      description,
+      photo,
+    }: {
+      id: string;
+      name: string;
+      description: string;
+      photo: string;
+    }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img
+          width="400"
+          height="250"
+          alt="location-reference"
+          src={`${photo}`}
+        />
+        <p>About this location:</p>
+        <p>{description}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  );
 }
 
-export default App
+export default App;
